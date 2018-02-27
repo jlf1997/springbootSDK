215f515f2cbc4324d335935defc3367883495c96
yx <xyang@163.com>
2018-02-27 14:47:24 +0800

    修改0.0.6 bug

--- demo/src/main/java/org/demo/DocumentationConfig.java
+++ demo/src/main/java/org/demo/DocumentationConfig.java
@@ -9,12 +9,13 @@ import org.springframework.stereotype.Component;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
-//@Component
+@Component
 @Primary
 public class DocumentationConfig implements SwaggerResourcesProvider {
     @Override
     public List<SwaggerResource> get() {
         List resources = new ArrayList<>();
+        resources.add(swaggerResource("default", "/v2/api-docs", "2.0"));
         resources.add(swaggerResource("订单系统", "/order/v2/api-docs", "2.0"));
         resources.add(swaggerResource("支付系统", "/pay/v2/api-docs", "2.0"));
         return resources;
